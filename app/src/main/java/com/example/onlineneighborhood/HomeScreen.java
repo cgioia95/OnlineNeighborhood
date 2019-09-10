b0481be9376751e9c247909dfcae5d4b35671b00
emmatysk <emma.tysk@gmail.com>
2019-09-10 06:41:01 +0200

    Added Adapter and recyclerview and logic to fetch data from the DB

--- app/src/main/java/com/example/onlineneighborhood/HomeScreen.java
+++ app/src/main/java/com/example/onlineneighborhood/HomeScreen.java
@@ -1,8 +1,11 @@
 package com.example.onlineneighborhood;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +16,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class HomeScreen extends AppCompatActivity implements View.OnClickListener {
 
@@ -23,10 +33,23 @@ public class HomeScreen extends AppCompatActivity implements View.OnClickListene
     String suburb;
 
     private FirebaseAuth fireBaseAuth;
+    DatabaseReference databaseEvents;
+
+    //Setting up recyclerview and adapter for displaying events
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<Event> eventList = new ArrayList<>();
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        databaseEvents = FirebaseDatabase.getInstance().getReference("events");
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
@@ -51,6 +74,43 @@ public class HomeScreen extends AppCompatActivity implements View.OnClickListene
 
         suburbTextView.setText(suburb);
         addEvent.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        databaseEvents.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                eventList.clear();
+                for(DataSnapshot eventSnapShot : dataSnapshot.getChildren()) {
+                    Event event = eventSnapShot.getValue(Event.class);
+
+                    eventList.add(event);
+                    Log.d("DISPLAY EVENT DATE", event.getDate());
+
+                }
+                //SET UP EVENTLIST
+
+                mRecyclerView = findViewById(R.id.recyclerView);
+                mLayoutManager = new LinearLayoutManager(HomeScreen.this);
+                mAdapter = new EventAdapter(eventList, HomeScreen.this);
+
+                mRecyclerView.setLayoutManager(mLayoutManager);
+                mRecyclerView.setAdapter(mAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 
