60350d191a1cd1295bf6eaebad2c25186d79fc8e
praja <jangleprajakta2295@gmail.com>
2019-09-16 18:29:50 +1000

    Pull request

---
+++ app/src/main/java/com/example/onlineneighborhood/Suburb.java
@@ -0,0 +1,46 @@
+package com.example.onlineneighborhood;
+
+import java.util.ArrayList;
+
+public class Suburb {
+    private String id;
+    private String subName;
+    private String postCode;
+    private ArrayList<Event> events;
+
+
+    public Suburb(){
+
+    }
+
+    public Suburb(String id, String subName, String postCode, ArrayList<Event> events) {
+        this.id = id;
+        this.subName = subName;
+        this.postCode = postCode;
+        this.events = events;
+    }
+
+    public String getSubName() {
+        return subName;
+    }
+
+    public void setSubName(String subName) {
+        this.subName = subName;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public ArrayList<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(ArrayList<Event> events) {
+        this.events = events;
+    }
+}
--- app/src/main/java/com/example/onlineneighborhood/UserInformation.java
+++ app/src/main/java/com/example/onlineneighborhood/UserInformation.java
@@ -2,23 +2,47 @@ package com.example.onlineneighborhood;
 
 public class UserInformation {
 
+    public String uid;
     public String name;
     public String preference;
-    public String bio;
-    public String dob;
 
     public UserInformation(){
 
     }
-    public UserInformation(String name, String preference, String dob, String bio) {
-        this.name = name;
-        this.preference = preference;
-        this.bio = bio;
-        this.dob = dob;
-    }
 
     public UserInformation(String name, String preference) {
         this.name = name;
+        this.preference = preference;
+    }
+
+
+    public UserInformation(String uid, String name, String preference) {
+        this.uid = uid;
+        this.name = name;
+        this.preference = preference;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPreference() {
+        return preference;
+    }
+
+    public void setPreference(String preference) {
         this.preference = preference;
     }
 }
