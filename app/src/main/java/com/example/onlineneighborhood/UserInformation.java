5820dbf38467d688a4db648933172f5883e84480
praja <jangleprajakta2295@gmail.com>
2019-09-17 15:12:23 +1000

    profile in Bottom bar

--- app/src/main/java/com/example/onlineneighborhood/UserInformation.java
+++ app/src/main/java/com/example/onlineneighborhood/UserInformation.java
@@ -5,6 +5,8 @@ public class UserInformation {
     public String uid;
     public String name;
     public String preference;
+    public String dob;
+    public String bio;
 
     public UserInformation(){
 
@@ -22,6 +24,13 @@ public class UserInformation {
         this.preference = preference;
     }
 
+    public UserInformation(String name, String preference, String dob, String bio) {
+        this.name = name;
+        this.preference = preference;
+        this.dob = dob;
+        this.bio = bio;
+    }
+
     public String getUid() {
         return uid;
     }
@@ -47,33 +56,6 @@ public class UserInformation {
     }
 
 
-    public UserInformation(String uid, String name, String preference) {
-        this.uid = uid;
-        this.name = name;
-        this.preference = preference;
-    }
 
-    public String getUid() {
-        return uid;
-    }
 
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPreference() {
-        return preference;
-    }
-
-    public void setPreference(String preference) {
-        this.preference = preference;
-    }
 }
